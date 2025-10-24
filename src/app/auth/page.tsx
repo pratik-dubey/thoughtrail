9452c73bbb999465c4801ea052734a0846859239
Pratik Dubey <pratikdubey091@gmail.com>
2025-10-24 22:34:22 +0530

    catching up with the auth component and middlware

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,5 @@
+import AuthLayout from "@/components/auth/auth-layout";
+
+export default function Auth() {
+  return <AuthLayout />;
+}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,56 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 export default function Header() {
-  return <div>Header</div>;
+  const router = useRouter();
+  const navItems = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Create",
+      href: "/post/create",
+    },
+  ];
+  return (
+    <header className="z-10 border-b bg-background sticky top-0 ">
+      <div className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <div className="flex items-center gap-6">
+          <Link href={"/"} className="font-bold text-xl">
+            Thought Rail
+          </Link>
+
+          <nav className="hidden md:flex items-center gap-6">
+            {navItems.map((item) => (
+              <Link
+                id={item.href}
+                href={item.href}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-primary"
+                )}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+        <div>
+          <div className="hidden md: block">
+            {" "}
+            {/*placeholder for search bar*/}
+          </div>
+          <div className="flex items-center gap-2">
+            <Button
+              onClick={() => router.push("/auth")}
+              className="cursor-pointer"
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
